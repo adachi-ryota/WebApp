f2593402eb7fc5dbbf4075d9564aacda474884be
housePC <a001ffu@DESKTOP-3MQ5EGR.localdomain>
2023-06-05 21:57:08 +0900

    flask start

--- SampleWebPage/main.py
+++ SampleWebPage/main.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='.', static_url_path='')
+app = Flask(__name__)
+
+bullets = [
+    '箇条書き1',
+    '箇条書き2',
+    '箇条書き3',
+    '箇条書き4',
+    '箇条書き5',
+    '箇条書き6'
+]
 
 @app.route('/')
 def home():
-    return app.send_static_file('HTML/index.html')
-
-app.run(port=9999, debug=True)
+    return render_template('index.html', bullets=bullets)
